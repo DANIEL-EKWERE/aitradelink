2bf1e17ab8a4e8d5f5f3a849ed4fd72d0722f3f7
DANIEL-EKWERE <102237731+DANIEL-EKWERE@users.noreply.github.com>
2025-07-09 12:06:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,31 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 # exist on error
 
+# set -o errexit
+
+# pip install -r requirements.txt
+
+# pip install gunicorn
+
+# python manage.py collectstatic --no-input
+
+# python manage.py migrate
+
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Upgrade pip and install build dependencies
+pip install --upgrade pip setuptools wheel
 
+# Install requirements with binary preference for Pillow
+pip install --only-binary=Pillow -r requirements.txt
+
+# Install gunicorn (or add it to requirements.txt)
 pip install gunicorn
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Run migrations
 python manage.py migrate
